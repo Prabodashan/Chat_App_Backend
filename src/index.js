9cf12db50fdfd334f12551ad98d1014ca9ecdb73
Prabodashan <prabodashan14@gmail.com>
2024-05-28 13:25:49 +0530

    Added middlwates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import dotenv from "dotenv";
-
 import app from "./app.js";
-
-//dotEnv config
-dotenv.config()
 
 //env variables
 const PORT = process.env.PORT || 3300;
-
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
