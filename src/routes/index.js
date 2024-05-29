105ebfb670d64108f494ff45dd0287b89f2b3bd2
Prabodashan <prabodashan14@gmail.com>
2024-05-29 18:30:06 +0530

    Add user search api endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 import authRoutes from "./auth.route.js";
+import useRoutes from "./user.route.js";
 import conversationRoutes from "./conversation.route.js";
 import messageRoutes from "./message.route.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/user", useRoutes);
 router.use("/conversation", conversationRoutes);
 router.use("/message", messageRoutes);
 
