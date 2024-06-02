1f6fe47af94abd274fad7e3ed2bdd753581ac59d
Prabodashan <prabodashan14@gmail.com>
2024-06-02 17:08:45 +0530

    Added socket.io connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Server } from "socket.io";
 
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
@@ -32,6 +33,18 @@ let server;
 
 server = app.listen(PORT, () => {
   logger.info(`Server is listening at ${PORT}.`);
+});
+
+//socket io
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CLIENT_ENDPOINT,
+  },
+});
+
+io.on("connection", (socket) => {
+  logger.info("socket io connected successfully.");
 });
 
 //handle server errors
